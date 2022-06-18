5de14892c2dd2dec477f5274a81d7b35076fa2b2
Daniel Lee <daniellee0217@yahoo.com>
2022-06-18 16:27:52 -0400

    Added starting character and db capability

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,12 +1,86 @@
+import { PrismaClient } from "@prisma/client";
 import { CommandInteraction } from "discord.js";
 import { SlashCommand } from ".";
 
+const prisma = new PrismaClient();
+
 export const startCommand: SlashCommand = {
+  // Description of command
   commandInfo: {
     name: "start",
     description: "Starts your character",
   },
+
+  // Called when command is executed
   executeSlashCommand: async (interaction: CommandInteraction) => {
-    interaction.reply("Creating a character!");
+    // Returns as there is no guild
+    if (!interaction.guild) return;
+
+    // Get data on guild and user
+    //const guild = interaction.guild.id;
+    const userID = interaction.user.id;
+
+    // Query DB if there is already a character entry with the same user ID
+    const existingCharacter = await prisma.character.findMany({
+      where: {
+        id: userID,
+      },
+    });
+
+    // If character entry already exists
+    if (existingCharacter.length > 0) {
+      interaction.reply("Character already exists.");
+
+      // If character does not exist, create one
+    } else {
+      console.log("Adding character!");
+
+      // Add new character to database
+      await prisma.character.create({
+        data: {
+          id: userID,
+          stats: {
+            create: {
+              id: 0,
+            },
+          },
+          currency: {
+            create: {
+              id: 0,
+              balance: 0,
+            },
+          },
+          activebuffs: {
+            create: {
+              id: 0,
+            },
+          },
+          inventory: {
+            create: {
+              id: 0,
+            },
+          },
+          equipment: {
+            create: {
+              id: 0,
+              head: "",
+              neck: "",
+              chest: "",
+              legs: "",
+              feet: "",
+              hands: "",
+              cape: "",
+            },
+          },
+        },
+      });
+
+      // Print completion to user
+      interaction.reply("Character creation complete!");
+    }
+
+    // Finds and prints all characters in database to log
+    //const allUsers = await prisma.character.findMany();
+    //console.log(allUsers);
   },
 };
